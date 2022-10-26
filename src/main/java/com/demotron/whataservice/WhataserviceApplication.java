8cf9f5e28582eac34d850a190bd6f3533a964751
Jessica Kerr <jessitron@honeycomb.io>
2022-10-25 18:19:29 -0700

    start.spring.io

---
+++ src/main/java/com/demotron/whataservice/WhataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.demotron.whataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhataserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demotron/whataservice/WhataserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotron.whataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhataserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
