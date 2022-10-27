b27ff130dbba117da7f9ebb2c54cb58498fd2eda
Jessica Kerr <jessitron@honeycomb.io>
2022-10-27 13:43:45 -0700

    Trying to do something clever
    it isn't working, i'm not finding the thing

--- src/main/java/com/demotron/whataservice/FraudCheckController.java
+++ src/main/java/com/demotron/whataservice/FraudCheckController.java
@@ -1,8 +1,13 @@
 package com.demotron.whataservice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,15 +36,37 @@ public class FraudCheckController {
 	@PostMapping(value = "/",
 		 produces = "application/json")
 	@ResponseBody 
-	public Map<String,Object> index(@RequestBody ChargeRequest input) {
+	public Map<String,Object> index(@RequestBody ChargeRequest input) throws IOException {
 		var out = new HashMap<String, Object>();
 		out.put("currencyCode", input.amount.currencyCode);
-		out.put("sus", beSerious(input.amount.currencyCode));
+		out.put("sus", beSerious(input.amount.currencyCode).equals(OK));
+		out.put("message", beSerious(input.amount.currencyCode));
 		return out;
 	}
 
+	private static final String OK = "Can't complain";
 	@WithSpan
-	public boolean beSerious(@SpanAttribute("app.currencyCode") String currencyCode) {
-		return false;
+	public String beSerious(@SpanAttribute("app.currencyCode") String currencyCode) throws IOException {
+
+		Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/ISO_4217").get();
+		System.out.println(doc.title());
+		Elements currencyCodeRows = doc.select("td:containsOwn(CAD)");
+		for (Element currencyCodeTd : currencyCodeRows) {
+			String moreInfoUrl = currencyCodeTd.nextElementSibling().nextElementSibling().nextElementSibling().select("a[href]").first().absUrl("href");
+			Document moreInfoDoc = Jsoup.connect(moreInfoUrl).get();
+			String check = distrustHighInflation(moreInfoDoc.body().text());
+			if (check != null) {
+				return check;
+			}
+		}
+		return OK;
 	}
+
+	private String distrustHighInflation(String information) {
+     if (information.matches("high inflation") || information.matches("hyperinflation") ) {
+			return "Oh, that currency is high inflation";
+		 }
+		 return null;
+	}
+
 }
