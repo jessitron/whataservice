db0da64f235a0d5062cd08b1e437445ecdf5ec27
Jessica Kerr <jessitron@honeycomb.io>
2022-10-27 14:32:59 -0700

    add another check

--- src/main/java/com/demotron/whataservice/FraudCheckFunctions.java
+++ src/main/java/com/demotron/whataservice/FraudCheckFunctions.java
@@ -9,7 +9,7 @@ import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 public class FraudCheckFunctions {
 
-  public static List<Function<String,String>> allChecks = List.of(FraudCheckFunctions::distrustHighInflation);
+  public static List<Function<String,String>> allChecks = List.of(FraudCheckFunctions::distrustHighInflation, FraudCheckFunctions::distrustSocialism);
 
   @WithSpan("check for hyperinflation")
 	public static String distrustHighInflation(@SpanAttribute("parameter.information") String information) {
@@ -17,6 +17,16 @@ public class FraudCheckFunctions {
 		span.setAttribute("app.information", information);
 		if (information.matches(".*high inflation.*") || information.matches(".*hyperinflation.*") ) {
 			return "Oh, that currency is high inflation";
+		}
+		return null;
+	}
+
+  @WithSpan("check for socialism")
+	public static String distrustSocialism(@SpanAttribute("parameter.information") String information) {
+		Span span = Span.current();
+		span.setAttribute("app.information", information);
+		if ( information.matches(".*socialism.*") ) {
+			return "Oh, that currency can't be trusted";
 		}
 		return null;
 	}
