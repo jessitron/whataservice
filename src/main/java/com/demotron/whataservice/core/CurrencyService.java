0a5b4f1539aa922a391fae02019f9b8a5e9216f6
Sargis Harutyunyan <sargis.harutyunyan@webbfontaine.com>
2022-10-28 23:53:32 +0400

    Add error handling

--- src/main/java/com/demotron/whataservice/core/CurrencyService.java
+++ src/main/java/com/demotron/whataservice/core/CurrencyService.java
@@ -13,12 +13,14 @@
  */
 package com.demotron.whataservice.core;
 
+import java.io.IOException;
 import java.util.Objects;
 import java.util.function.Function;
 
 import com.demotron.whataservice.otel.OtelConfiguration;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Scope;
 import org.jsoup.Jsoup;
@@ -57,45 +59,48 @@ public class CurrencyService {
         Span span = tracer.spanBuilder("call wiki for currency").startSpan();
 
         // Make the span the current span
-        try (Scope ss = span.makeCurrent()) {
+        try (Scope scope = span.makeCurrent()) {
             return doPerformOneCheck(currencyCode, checker);
+        } catch (Exception ex) {
+            LOGGER.error("", ex);
+
+            span.setStatus(StatusCode.ERROR, "Problem with currency processing");
+            span.recordException(ex);
+
+            return "Failed to check";
         } finally {
             span.end();
         }
     }
 
-    private static String doPerformOneCheck(String currencyCode, Function<String, String> checker) {
-        try {
-            Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/ISO_4217").get();
-            LOGGER.info("Doc title: {}", doc.title());
+    private static String doPerformOneCheck(String currencyCode, Function<String, String> checker) throws IOException {
+        Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/ISO_4217").get();
+        LOGGER.info("Doc title: {}", doc.title());
 
-            Elements currencyCodeRows = doc.select("td:containsOwn(" + currencyCode + ')');
-            for (Element currencyCodeTd : currencyCodeRows) {
-                try {
-                    String moreInfoUrl = currencyCodeTd
-                        .nextElementSibling()
-                        .nextElementSibling()
-                        .nextElementSibling()
-                        .select("a[href]")
-                        .first()
-                        .absUrl("href");
+        Elements currencyCodeRows = doc.select("td:containsOwn(" + currencyCode + ')');
+        for (Element currencyCodeTd : currencyCodeRows) {
+            try {
+                String moreInfoUrl = currencyCodeTd
+                    .nextElementSibling()
+                    .nextElementSibling()
+                    .nextElementSibling()
+                    .select("a[href]")
+                    .first()
+                    .absUrl("href");
 
-                    LOGGER.info("More info url: {}", moreInfoUrl);
+                LOGGER.info("More info url: {}", moreInfoUrl);
 
-                    Document moreInfoDoc = Jsoup.connect(moreInfoUrl).get();
-                    String check = checker.apply(moreInfoDoc.body().text());
-                    if (check != null) {
-                        return check;
-                    }
-                } catch (Exception e) {
-                    LOGGER.trace("Cannot parse", e);
-                    // well, there might be another currencyCodeRow that works, so keep going
+                Document moreInfoDoc = Jsoup.connect(moreInfoUrl).get();
+                String check = checker.apply(moreInfoDoc.body().text());
+                if (check != null) {
+                    return check;
                 }
+            } catch (Exception e) {
+                LOGGER.trace("Cannot parse", e);
+                // well, there might be another currencyCodeRow that works, so keep going
             }
-        } catch (Exception e) {
-            LOGGER.error("", e);
-            return "Failed to check";
         }
+
         return null;
     }
 }
